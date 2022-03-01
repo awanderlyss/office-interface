e75f7f1ba41714b7083f82334c746c46a02fe55e
Alyssa Felix <alyssa.felix7@gmail.com>
2022-03-01 17:11:08 -0500

    Create four box grid and add pacific time to box 4

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function changeTimezone(date, ianatz) {
+
+    // suppose the date is 12:00 UTC
+    var invdate = new Date(date.toLocaleString('en-US', {
+        timeZone: ianatz
+    }));
+
+    // then invdate will be 07:00 in Toronto
+    // and the diff is 5 hours
+    var diff = date.getTime() - invdate.getTime();
+
+    // so 12:00 in Toronto is 17:00 UTC
+    return new Date(date.getTime() - diff); // needs to substract
+
+}
+  
+function currentTime() {
+    let date = new Date();
+    let westCoast = changeTimezone(date, "America/Tijuana");
+    let hh = westCoast.getHours();
+    let mm = westCoast.getMinutes();
+    let ss = westCoast.getSeconds();
+    let session = "am";
+
+    if(hh == 0){
+        hh = 12;
+    }
+    if(hh > 12){
+        hh = hh - 12;
+        session = "pm";
+        }
+
+        hh = (hh < 10) ? "0" + hh : hh;
+        mm = (mm < 10) ? "0" + mm : mm;
+        ss = (ss < 10) ? "0" + ss : ss;
+        
+        let time = hh + " : " + mm + " : " + ss + " " + session;
+
+    document.getElementById("currentTime").innerHTML = time;
+    let t = setTimeout(function(){ currentTime() }, 1000);
+}
+currentTime();
